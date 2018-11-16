18acf77fc72b02d6e876dffcc062f1469c97e2dc
Dennis <deweis@gmx.ch>
2018-11-16 11:03:19 +0100

    Add Display and KeyPad components to the App container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Display from './components/display';
+import KeyPad from './components/keypad';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App container">
+        <div className="row">
+          <table className="table">
+            <tbody>
+              <Display />
+              <KeyPad />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
   }
 }
 
