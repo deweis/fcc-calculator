9876793953c738b6dae17c07b47a3737968e4e96
Dennis <deweis@gmx.ch>
2018-11-16 11:03:48 +0100

    Add KeyPad Layout

---
+++ src/components/keypad.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Aux from '../hoc/Aux_/Aux_';
+import './keypad.css';
+
+const keypad = props => {
+  return (
+    <Aux>
+      <tr>
+        <td className="key">AC</td>
+        <td className="key">CE</td>
+        <td className="key" />
+        <td className="key">*</td>
+      </tr>
+      <tr>
+        <td className="key">7</td>
+        <td className="key">8</td>
+        <td className="key">9</td>
+        <td className="key">/</td>
+      </tr>
+      <tr>
+        <td className="key">4</td>
+        <td className="key">5</td>
+        <td className="key">6</td>
+        <td className="key">+</td>
+      </tr>
+      <tr>
+        <td className="key">1</td>
+        <td className="key">2</td>
+        <td className="key">3</td>
+        <td className="key">-</td>
+      </tr>
+      <tr>
+        <td className="key">-</td>
+        <td className="key">0</td>
+        <td className="key">.</td>
+        <td className="key">=</td>
+      </tr>
+    </Aux>
+  );
+};
+
+export default keypad;
