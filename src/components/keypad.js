e5f8f7375c9d264125cb37165b8101720e526ecf
Dennis <deweis@gmx.ch>
2018-11-23 10:16:30 +0100

    Solve user story #2

--- src/components/keypad.js
+++ src/components/keypad.js
@@ -18,13 +18,13 @@ const keypad = props => {
         </td>
       </tr>
       <tr>
-        <td className="key" onClick={() => props.numberClicked(7)}>
+        <td id="seven" className="key" onClick={() => props.numberClicked(7)}>
           7
         </td>
-        <td className="key" onClick={() => props.numberClicked(8)}>
+        <td id="eight" className="key" onClick={() => props.numberClicked(8)}>
           8
         </td>
-        <td className="key" onClick={() => props.numberClicked(9)}>
+        <td id="nine" className="key" onClick={() => props.numberClicked(9)}>
           9
         </td>
         <td className="key" onClick={() => props.operatorClicked('/')}>
@@ -32,13 +32,13 @@ const keypad = props => {
         </td>
       </tr>
       <tr>
-        <td className="key" onClick={() => props.numberClicked(4)}>
+        <td id="four" className="key" onClick={() => props.numberClicked(4)}>
           4
         </td>
-        <td className="key" onClick={() => props.numberClicked(5)}>
+        <td id="five" className="key" onClick={() => props.numberClicked(5)}>
           5
         </td>
-        <td className="key" onClick={() => props.numberClicked(6)}>
+        <td id="six" className="key" onClick={() => props.numberClicked(6)}>
           6
         </td>
         <td className="key" onClick={() => props.operatorClicked('+')}>
@@ -46,13 +46,13 @@ const keypad = props => {
         </td>
       </tr>
       <tr>
-        <td className="key" onClick={() => props.numberClicked(1)}>
+        <td id="one" className="key" onClick={() => props.numberClicked(1)}>
           1
         </td>
-        <td className="key" onClick={() => props.numberClicked(2)}>
+        <td id="two" className="key" onClick={() => props.numberClicked(2)}>
           2
         </td>
-        <td className="key" onClick={() => props.numberClicked(3)}>
+        <td id="three" className="key" onClick={() => props.numberClicked(3)}>
           3
         </td>
         <td className="key" onClick={() => props.operatorClicked('-')}>
@@ -61,7 +61,7 @@ const keypad = props => {
       </tr>
       <tr>
         <td className="key">-</td>
-        <td className="key" onClick={() => props.numberClicked(0)}>
+        <td id="zero" className="key" onClick={() => props.numberClicked(0)}>
           0
         </td>
         <td className="key">.</td>
